ec272a77a4f5f8d31d60a7ef724b78e7c78e8dc3
Neville <nevillenieman99@gmail.com>
2023-06-09 10:28:20 +0200

    add argument parsing

--- interactive.py
+++ interactive.py
@@ -4,143 +4,163 @@ import ownFuncs.funcs as of
 import ownFuncs.optimizationFuncs as opt
 from ownFuncs.shapes import Shapes
 import ownFuncs.colorspacePlotter as csplt
-
-DEBUG = 0
-CSPLOT = 0
-
-if DEBUG:
-    of.cleanDir("data/solveanimation")
-    of.cleanDir("data/debug_imgs")
-
-# Connect to phone adb
-client = AdbClient(host="127.0.0.1", port=5037)
-devices = client.devices()
-device = devices[0]
-
-# create and save sreenshot
-result = device.screencap()
-src = "data/hue_scrambled_screen.png"
-with open(src, "wb") as fp:
-    fp.write(result)
-fp.close()
-PUZZLE_ID = "screen"
-
-img = cv2.imread(src)
-scaler = 0.5
-img = of.scaleImg(img, scaler)
-assert img is not None, "file could not be read"
-
-shapes = Shapes(img, PUZZLE_ID, reduce_factor=scaler)
-print(
-    f"solving puzzle with {len(shapes.all)} shapes of which {len(shapes.unlocked)} are unlocked"
-)
-
-if CSPLOT:
-    csplt.ionshow()
-    csplt.rgb_space_plot(shapes.all)
-    csplt.rgb_space_plot(shapes.all, space="HSV", title="HSV plot")
-    csplt.rgb_space_plot(shapes.all, space="LAB", title="LAB plot")
-    csplt.xy_rgb_space_plot(shapes.all, title="Before ordening")
-
-order: int = 1
-datas, MGs, Cs = opt.fitSurface(shapes, order)
-opt.setShapeColorEstimations(shapes, Cs, order)
-
-stepcount: int = -1
-num_free_shapes = len(shapes.unlocked)
-remaining_color_est_error = 0
-
-while len(shapes.unlocked) > 1 and True:
-    stepcount += 1
-    shape = max(shapes.unlocked, key=lambda s: s.distToEstimation)
-    BGR = opt.color_at_xy(Cs, shape.tapX, shape.tapY, order)
-    closestShape, err = shapes.findShapeClosestToColor(BGR, shape)
-    remaining_color_est_error += err
-    shapes.swapShapes(shape, closestShape)
-
-    if not shape.same_as(closestShape) and DEBUG:
-        shapes.markSwappedShapes(shape, closestShape)
-        of.saveImg(shapes.img, "data/solveanimation/", f"order1_{stepcount}.png")
-
-order = 2
-somethingChanged = True
-limit = 0
-
-refit = False
-while limit < 2 * len(shapes.all) and True:
-    if limit % 10 == 0 or len(shapes.unlocked) == 0 or not somethingChanged:
-        shapes.reset_locks()
-        opt.determine_close_to_estimate(shapes)
-        datas2, MGs, Cs = opt.fitSurface(shapes, order, False)
-        opt.setShapeColorEstimations(shapes, Cs, order)
-        if refit:
-            break
-        refit = True
-    else:
-        refit = False
-
-    somethingChanged = False
-    limit += 1
-    stepcount += 1
-
-    shape = opt.get_largest_error_shape(shapes.unlocked)
-    BGR = opt.color_at_xy(Cs, shape.tapX, shape.tapY, order)
-    closestShape, err = shapes.findShapeClosestToColor(BGR, shape)
-    shapes.swapShapes(shape, closestShape)
-
-    # Check if shape has swapped with itself
-    if not shape.same_as(closestShape):
-        somethingChanged = True
-        if DEBUG:
-            shapes.markSwappedShapes(shape, closestShape)
-            of.saveImg(shapes.img, "data/solveanimation/", f"order2_{stepcount}.png")
+import argparse
 
 
-if DEBUG:
-    shapes.updateImg(False)
-    of.saveImg(shapes.img, "data/solveanimation/", f"end1_{stepcount}.png")
-    shapes.draw_voronoi(draw_lines=True, draw_centroids=True)
-    of.saveImg(shapes.voronoi_img, "data/voronoi/", f"P{PUZZLE_ID}.png")
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Enable debugging mode",
+    )
+    parser.add_argument(
+        "--csplot",
+        action="store_true",
+        help="Draws a view colorspace plots during process",
+    )
+    args = parser.parse_args()
+    return args
 
-if CSPLOT:
-    shapes.reset_locks()
-    csplt.xy_rgb_space_plot(shapes.all, title="After ordening")
-    csplt.show()
 
-shapes.reset_locks()
-shapes.reset_colors()
+if __name__ == "__main__":
+    # Parse the arguments
+    args = parseArguments()
 
-stepcount = -1
-conv_hull_first = None
+    if args.debug:
+        of.cleanDir("data/solveanimation")
+        of.cleanDir("data/debug_imgs")
 
-while len(shapes.unlocked) > 1 and stepcount < 2 * len(shapes.all):
-    largestShapes = shapes.get_largest_shapes()
-    # largestShapes = shapes.unlocked
-    shape = min(largestShapes, key=lambda s: s.tapX)
-    while len(largestShapes) > 0:
-        shape = shapes.next_convex_hull_shape(shape, largestShapes)
+    # Connect to phone adb
+    client = AdbClient(host="127.0.0.1", port=5037)
+    devices = client.devices()
+    device = devices[0]
+
+    # create and save sreenshot
+    result = device.screencap()
+    src = "data/hue_scrambled_screen.png"
+    with open(src, "wb") as fp:
+        fp.write(result)
+    fp.close()
+    PUZZLE_ID = "screen"
+
+    img = cv2.imread(src)
+    scaler = 0.5
+    img = of.scaleImg(img, scaler)
+    assert img is not None, "file could not be read"
+
+    shapes = Shapes(img, PUZZLE_ID, reduce_factor=scaler)
+    print(
+        f"solving puzzle with {len(shapes.all)} shapes of which {len(shapes.unlocked)} are unlocked"
+    )
+
+    if args.csplot:
+        csplt.ionshow()
+        csplt.rgb_space_plot(shapes.all)
+        csplt.rgb_space_plot(shapes.all, space="HSV", title="HSV plot")
+        csplt.rgb_space_plot(shapes.all, space="LAB", title="LAB plot")
+        csplt.xy_rgb_space_plot(shapes.all, title="Before ordening")
+
+    order: int = 1
+    datas, MGs, Cs = opt.fitSurface(shapes, order)
+    opt.setShapeColorEstimations(shapes, Cs, order)
+
+    stepcount: int = -1
+    num_free_shapes = len(shapes.unlocked)
+    remaining_color_est_error = 0
+
+    while len(shapes.unlocked) > 1 and True:
         stepcount += 1
-        swap_shape = shapes.find_shape_by_color(shape.end_color)
-        if swap_shape is not None:
-            if not shape.same_as(swap_shape):
-                shapes.swapShapes(shape, swap_shape, device, 0.01)
-            else:
-                # mark as locked but dont fysically swap
-                shapes.swapShapes(shape, swap_shape)
+        shape = max(shapes.unlocked, key=lambda s: s.distToEstimation)
+        BGR = opt.color_at_xy(Cs, shape.tapX, shape.tapY, order)
+        closestShape, err = shapes.findShapeClosestToColor(BGR, shape)
+        remaining_color_est_error += err
+        shapes.swapShapes(shape, closestShape)
 
-        if shape in largestShapes:
-            largestShapes.remove(shape)
+        if not shape.same_as(closestShape) and args.debug:
+            shapes.markSwappedShapes(shape, closestShape)
+            of.saveImg(shapes.img, "data/solveanimation/", f"order1_{stepcount}.png")
 
-        if DEBUG:
-            shapes.markSwappedShapes(shape, swap_shape)
-            shapes.draw_convex_hull()
-            of.saveImg(
-                shapes.img,
-                "data/solveanimation/",
-                f"step_{stepcount}.png",
-            )
+    order = 2
+    somethingChanged = True
+    limit = 0
 
-if DEBUG:
-    shapes.updateImg(False)
-    shapes.draw_convex_hull()
-    of.saveImg(shapes.img, "data/solveanimation/", f"end2_{stepcount}.png")
+    refit = False
+    while limit < 2 * len(shapes.all) and True:
+        if limit % 10 == 0 or len(shapes.unlocked) == 0 or not somethingChanged:
+            shapes.reset_locks()
+            opt.determine_close_to_estimate(shapes)
+            datas2, MGs, Cs = opt.fitSurface(shapes, order, False)
+            opt.setShapeColorEstimations(shapes, Cs, order)
+            if refit:
+                break
+            refit = True
+        else:
+            refit = False
+
+        somethingChanged = False
+        limit += 1
+        stepcount += 1
+
+        shape = opt.get_largest_error_shape(shapes.unlocked)
+        BGR = opt.color_at_xy(Cs, shape.tapX, shape.tapY, order)
+        closestShape, err = shapes.findShapeClosestToColor(BGR, shape)
+        shapes.swapShapes(shape, closestShape)
+
+        # Check if shape has swapped with itself
+        if not shape.same_as(closestShape):
+            somethingChanged = True
+            if args.debug:
+                shapes.markSwappedShapes(shape, closestShape)
+                of.saveImg(
+                    shapes.img, "data/solveanimation/", f"order2_{stepcount}.png"
+                )
+
+    if args.debug:
+        shapes.updateImg(False)
+        of.saveImg(shapes.img, "data/solveanimation/", f"end1_{stepcount}.png")
+        shapes.draw_voronoi(draw_lines=True, draw_centroids=True)
+        of.saveImg(shapes.voronoi_img, "data/voronoi/", f"P{PUZZLE_ID}.png")
+
+    if args.csplot:
+        shapes.reset_locks()
+        csplt.xy_rgb_space_plot(shapes.all, title="After ordening")
+        csplt.show()
+
+    shapes.reset_locks()
+    shapes.reset_colors()
+
+    stepcount = -1
+    conv_hull_first = None
+
+    while len(shapes.unlocked) > 1 and stepcount < 2 * len(shapes.all):
+        largestShapes = shapes.get_largest_shapes()
+        # largestShapes = shapes.unlocked
+        shape = min(largestShapes, key=lambda s: s.tapX)
+        while len(largestShapes) > 0:
+            shape = shapes.next_convex_hull_shape(shape, largestShapes)
+            stepcount += 1
+            swap_shape = shapes.find_shape_by_color(shape.end_color)
+            if swap_shape is not None:
+                if not shape.same_as(swap_shape):
+                    shapes.swapShapes(shape, swap_shape, device, 0.01)
+                else:
+                    # mark as locked but dont fysically swap
+                    shapes.swapShapes(shape, swap_shape)
+
+            if shape in largestShapes:
+                largestShapes.remove(shape)
+
+            if args.debug:
+                shapes.markSwappedShapes(shape, swap_shape)
+                shapes.draw_convex_hull()
+                of.saveImg(
+                    shapes.img,
+                    "data/solveanimation/",
+                    f"step_{stepcount}.png",
+                )
+
+    if args.debug:
+        shapes.updateImg(False)
+        shapes.draw_convex_hull()
+        of.saveImg(shapes.img, "data/solveanimation/", f"end2_{stepcount}.png")
--- main.py
+++ main.py
@@ -3,39 +3,54 @@ import ownFuncs.funcs as of
 from ownFuncs.shapes import Shapes
 import ownFuncs.colorspacePlotter as plotter
 import ownFuncs.solveStrategies as ss
+from ppadb.client import Client as AdbClient
+
+# Connect to phone adb
+client = AdbClient(host="127.0.0.1", port=5037)
+devices = client.devices()
+device = devices[0]
 
 # run settings
-Npuzzle = "15"
+PUZZLE_ID = "15"
+take_screencap = True
 grabSolved = False
 skipSwap = False
 
 # load image
-if grabSolved:
-    src = f"data/hue_solved_{Npuzzle}.png"
-    Npuzzle += "s"
+if take_screencap:
+    result = device.screencap()
+    src = "data/hue_scrambled_screen.png"
+    with open(src, "wb") as fp:
+        fp.write(result)
+    fp.close()
+    PUZZLE_ID = "screen"
+elif grabSolved:
+    src = f"data/hue_solved_{PUZZLE_ID}.png"
+    PUZZLE_ID += "s"
 else:
-    src = f"data/hue_scrambled_{Npuzzle}.png"
+    src = f"data/hue_scrambled_{PUZZLE_ID}.png"
+
 img = cv2.imread(src)
 assert img is not None, "file could not be read, check with os.path.exists()"
 
 # Reduce image size to ease computations
 img = of.scaleImg(img, maxHeight=1000, maxWidth=3000)
 
-shapes = Shapes(img, Npuzzle)
+shapes = Shapes(img, PUZZLE_ID)
 
 print("Start swapping?")
 cv2.waitKey(1)
 
 # Pick strategy for solving puzzle
 if not skipSwap:
-    ss.solve3(shapes, Npuzzle)
+    ss.solve3(shapes, PUZZLE_ID)
 
 # Wait with plotting before crashing opencv image window
 print("Draw plots? opencv window will stop reacting.")
 cv2.waitKey(0)
 
 # Show plots for analyzing results
-plotter.rgb_space_plot(shapes.all, drawConnections=True)
+plotter.rgb_space_plot(shapes.all)
 plotter.surfacePlot2(shapes.all)
 
 
